057b2e8ec0fd469b3a4a845a586a1e150a71a53a
Nijarudeen  Rajak <50949038+black-nijar@users.noreply.github.com>
2021-09-21 19:37:18 +0530

    adding navbar ui

---
+++ car-app/src/app/containers/HomePage/index.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import styled from "styled-components";
+import tw from "twin.macro";
+import { NavBar } from "../../components/navbar";
+
+const PageContainer = styled.div`
+  ${tw`
+    flex
+    flex-col
+    w-full
+    h-full
+    items-center
+    overflow-x-hidden  
+  `}
+`;
+
+export const HomePage = () => {
+  return (
+    <PageContainer>
+      <NavBar />
+    </PageContainer>
+  );
+};
